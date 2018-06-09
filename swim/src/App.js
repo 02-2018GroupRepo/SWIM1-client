9daa29c67ae93c2f3f932d23c84967f0413d08f4
Noelle <noelle.r.irvin@gmail.com>
2018-06-09 11:18:50 -0400

    Login and admin feature

--- swim/src/App.js
+++ swim/src/App.js
@@ -6,16 +6,35 @@ import DockDoor from './Component/DockDoor';
 import ASNSearch from './Component/ASNSearch';
 import ASNDisplay from './Component/ASNDisplay';
 import ASNReceived from './Component/ASNReceived';
+import Login from './Component/Login.js'
 import { Route, Link } from 'react-router-dom';
 import './index.css';
 import './App.css';
 
 class App extends Component {
+
+  constructor(){
+    super();
+    this.state ={
+      isAuth : false
+    }
+    this._isAuth = this._isAuth.bind(this);
+  }
+
+  _isAuth(){
+    console.log("changing state of is auth");
+    this.setState({
+      isAuth: true
+    })
+  }
+  
   render() {
+
     return (
       <div className="container App">
         <Route path="/" component={Header} />
-        <Route exact path="/" component={Home} />
+        <Route path="/" component={ (props)=> <Login props={props} auth={this.state.isAuth} isAuth={this._isAuth} />} />
+        <Route exact path="/" component={(props)=> <Home props={props} auth={this.state.isAuth} />} />
         <Route exact path= "/receiving" component={DockDoor} />
         <Route exact path="/outbound" component={ASNReceived} />
         <Route path="/" component={Footer} />
